665e3f28d3efe23af69945c903b60f75f1d5ba28
hs150521 <hs150521@163.com>
2024-05-19 18:53:42 +0800

    继续修改多进程

--- RMSDK/08_multi_final.py
+++ RMSDK/08_multi_final.py
@@ -1,6 +1,5 @@
 import cv2
-import queue
-from threading import Thread
+from multiprocessing import  Process, Queue
 import robomaster
 from robomaster import robot
 from ultralytics import YOLO
@@ -9,8 +8,9 @@ import time
 
 # 加载预训练的 YOLOv8 模型
 model = YOLO('../runs/best.pt')
-q=queue.Queue()
-cnt=0
+q=Queue()
+q_cnt=Queue()
+#cnt=0
 if __name__ == '__main__':
 
     ep_robot = robot.Robot()
@@ -21,13 +21,20 @@ if __name__ == '__main__':
     ep_camera = ep_robot.camera
     ep_camera.start_video_stream(display=False, resolution='360p')
 
-    def refer(name):
-        global cnt
+    def refer(name,q,q_cnt,model):
+        #global cnt
+        #from ultralytics import YOLO
+        #print("getting1")
+        #model = YOLO('../runs/best.pt')
+        #model = YOLO('/home/yuanzl/RM-yolo/runs/best.pt')
+        #print("getting2")
         while True:
             if(~q.empty()):
-                cnt+=1
+                #cnt+=1
+                q_cnt.put(name)
+                print("getting3")
                 results = model(q.get())
-
+                print("getting4")
                 for r in results:
                     boxes = r.boxes
 
@@ -43,7 +50,8 @@ if __name__ == '__main__':
 
                 annotated_frame = results[0].plot()
                 cv2.imshow("YOLOv8", annotated_frame)
-
+                
+                '''
                 posx = float(pos_arr[0])+40
                 posy = float(pos_arr[1])-20
 
@@ -54,37 +62,25 @@ if __name__ == '__main__':
                     ep_gimbal.move(yaw=yaw, pitch=pitch, pitch_speed=20, yaw_speed=20)
                     time.sleep(1)
                     ep_blaster.fire()
-
+                '''
+                
                 cv2.waitKey(1)
-            
-    t1 = Thread(target=refer,args="1")
-    t2 = Thread(target=refer,args="2")
-    t3 = Thread(target=refer,args="3")
-    t4 = Thread(target=refer,args="4")
-    t5 = Thread(target=refer,args="5")
-    t6 = Thread(target=refer,args="6")
     
-    q.put(ep_camera.read_cv2_image(strategy='newest'))
-    q.put(ep_camera.read_cv2_image(strategy='newest'))
-    q.put(ep_camera.read_cv2_image(strategy='newest'))
-    q.put(ep_camera.read_cv2_image(strategy='newest'))
-    q.put(ep_camera.read_cv2_image(strategy='newest'))
-    #q.put(ep_camera.read_cv2_image(strategy='newest'))
-    
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    #t6.start()
+    process_list = []
+    for i in range(5):  #开启子进程执行函数
+        q.put(ep_camera.read_cv2_image(strategy='newest'))
+        p = Process(target=refer,args=(str(i),q,q_cnt,model,)) #实例化进程对象
+        p.start()
+        process_list.append(p)
+ 
 
     while True:
-        if(cnt>0):
+        if(~q_cnt.empty()):
             q.put(ep_camera.read_cv2_image(strategy='newest'))
-            cnt-=1
+            print(q_cnt.get())
     
-    t1.join()
-    t2.join()
+    for i in process_list:
+        p.join()       
 
     cv2.destroyAllWindows()
     ep_camera.stop_video_stream()
